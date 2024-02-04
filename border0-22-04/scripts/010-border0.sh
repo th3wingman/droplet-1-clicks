da6f0c5b5b94d8dc15b23d94e2af033d9d56264b
The Greg <greg@border0.com>
2024-02-05 09:14:48 +1000

    expanding install and wizard scripts, adding cleanup

--- border0-22-04/scripts/010-border0.sh
+++ border0-22-04/scripts/010-border0.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-apt-get update && apt-get -y install gpg curl
+apt-get update
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.border0.com/deb/gpg | gpg --dearmor -o /etc/apt/keyrings/border0.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/border0.gpg] https://download.border0.com/deb/ stable main" > /etc/apt/sources.list.d/border0.list
 apt-get -y update
 apt-get -y install border0
 
+# install bash completion
+/usr/bin/border0 completion bash > /etc/bash_completion.d/border0 || true
+
+# make our install script executable
 chmod +x /sbin/border0-install.sh
+
+# 
+# Additional check for /etc/border0 directory and update /root/.bashrc
+bashrcLine="if [ -d /etc/border0 ]; then echo /sbin/border0-install.sh has already been executed; else /sbin/border0-install.sh; fi"
+if [ -d /etc/border0 ]; then
+    # Check if line already exists in /root/.bashrc to avoid duplicate entries
+    if ! grep -qxF -- "$bashrcLine" /root/.bashrc; then
+        echo "$bashrcLine" >> /root/.bashrc
+        echo "Check for /etc/border0 added to /root/.bashrc."
+    else
+        echo "Check for /etc/border0 already exists in /root/.bashrc."
+    fi
+fi
 #
-
-# this is required bot 999-img_check.sh to pass
-apt-get -y purge droplet-agent
-apt-get -y purge do-agent
-rm -fr /opt/digitalocean
-
-# clean up log files
-rm -frv /var/log/ubuntu-advantage.log
-rm -frv /var/log/kern.log
-rm -frv /var/log/auth.log
 ## end of 010-border0.sh
---
+++ border0-22-04/scripts/011-border0-clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this is required bot 999-img_check.sh to pass
+apt-get -y purge droplet-agent
+apt-get -y purge do-agent
+rm -fr /opt/digitalocean
+
+# clear out MOTD
+rm -frv /etc/update-motd.d/10-help-text
+rm -frv /etc/update-motd.d/50-motd-news
+rm -frv /etc/update-motd.d/50-landscape-sysinfo
+rm -frv /etc/update-motd.d/90-updates-available
+
+# clean up log files
+rm -frv /var/log/ubuntu-advantage.log
+rm -frv /var/log/kern.log
+rm -frv /var/log/auth.log
+
+## end of 011-border0-clean.sh
+
