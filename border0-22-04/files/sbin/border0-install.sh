4b9e8efcaeb1308ea5d72cf0504f3dd2f1cd8e94
The Greg <greg@border0.com>
2024-02-05 12:36:36 +1000

    additional help and info

--- border0-22-04/files/sbin/border0-install.sh
+++ border0-22-04/files/sbin/border0-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "Starting Border0 Connector Install Script"
+echo -e "\nStarting Border0 Connector Install Script\n"
 
 mkdir -p /opt/border0 || true
 touch /opt/border0/border0.install.log || true
@@ -14,7 +14,7 @@ Please consider closing off port 22 on your firewall
 
 Border0 Connector comes with build in Built-in SSH server
 https://docs.border0.com/docs/using-the-built-in-border0-ssh-server
-********************************************************************************
+
 EOF
 
 # ask user for input y or N
@@ -27,14 +27,18 @@ We will need to initiate doctl tooling and create API token for that
 EOF
 
 read -p "Would you like to enable Digital-Ocean Plugin and initialize doctl toolset? (y/N) " -n 1 -r
+echo # Ensure newline after input
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo -e "\nInitializing Digital-Ocean Tooling"
-    if [ ! -d "/root/.config" ]; then mkdir -p /root/.config ; fi
+    echo -e "\nInitializing Digital-Ocean Tooling\nWe will need to create API token for that\nhttps://docs.digitalocean.com/reference/api/create-personal-access-token/"
+    if [ ! -d "/root/.config" ]; then mkdir -p /root/.config; fi
     doctl auth init
+else
+    echo -e "\nSkipping Digital-Ocean Tooling Initialization\n You can always configure them alter with 'doctl auth init' command\n"
 
 fi
-echo <<EOF
+
+cat <<EOF
 ********************************************************************************
 Border0 Connector Install Script Completed
 
